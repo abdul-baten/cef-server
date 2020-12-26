f3f7de40a1932519230129501fd447265e720640
Abdul Baten <md.abdul@brainstation23.com>
2020-12-26 18:38:12 +0600

    add error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import errorHandlers from './middleware/errorHandlers';
 import express from 'express';
 import http from 'http';
 import middleware from './middleware';
@@ -6,6 +7,7 @@ import { applyMiddleware } from './utils';
 const app = express();
 
 applyMiddleware(middleware, app);
+applyMiddleware(errorHandlers, app);
 
 const server = http.createServer(app);
 
